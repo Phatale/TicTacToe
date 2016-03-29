30e61962ba119a7136dfaeff7c671e5d1aa770c7
Samir Fersobe <Samir Fersobe>
2016-03-29 14:02:10 -0400

    Commmiteando todo

--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,9 @@
 var res = {
     O_png : "res/O.png",
     X_png : "res/X.png",
-    tablero_png : "res/tablero.png"
+    tablero_png : "res/tablero.png",
+    reset_png: "res/reset.png",
+    resetPush_png: "res/resetPush.png"
 };
 
 var g_resources = [];
